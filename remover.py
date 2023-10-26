23d9c3f12f469a50bff63ecab36d1e05531b6897
Stephen Le <stephen.dt.le@gmail.com>
2023-10-26 14:49:10 -0400

    Added additional sheet in output to for bugged rows

--- remover.py
+++ remover.py
@@ -1,18 +1,22 @@
 from openpyxl import load_workbook, Workbook
 import os
 file_names = os.listdir('data-input')
-rows_to_delete = []
+bugged_rows = []
 for i in file_names:
     if i != '.gitignore':
         wb = load_workbook(filename='data-input/'+i)
-        ws = wb.active
-        for row in ws.iter_rows():
-            start_timestamp = row[4].value.split(":")[1]
-            end_timestamp = row[5].value.split(":")[1]
+        original_worksheet = wb.active
+        for row in original_worksheet.iter_rows():
+            start_timestamp = row[4].value.split(": ")[1]
+            end_timestamp = row[5].value.split(": ")[1]
             if start_timestamp == end_timestamp:
-                rows_to_delete.append(row[0].row)
-        for row_num in rows_to_delete:
-            ws.delete_rows(row_num)
+                bugged_rows.append(row)
+        for row in bugged_rows:
+            original_worksheet.delete_rows(row[0].row)
+        sheet_with_bugged_times = wb.create_sheet("bugged_rows")
+        for row in bugged_rows:
+            row_copy = [i.value for i in row]
+            sheet_with_bugged_times.append(row_copy)
         wb.save('data-output/'+i)
 
 
